2e72bb33e1bff048139765c98e7fb3f4ec2b1c1c
iambasantarai <developer.basanta@gmail.com>
2025-09-15 14:34:23 +0545

    Document modules/functions with docstring

--- reasoning/core.py
+++ reasoning/core.py
@@ -1,2 +1,5 @@
 def init_reasoning():
+    """
+    Initializes reasoning module.
+    """
     print("i'm from reasoning/core.py")
--- vision/core.py
+++ vision/core.py
@@ -2,17 +2,30 @@ import cv2 as cv
 import mediapipe as mp
 
 def init_vision():
+    """
+    Initializes vision module.
+    """
+    detect_face_landmark()
+
+def detect_face_landmark():
+    """
+    Detects face landmarks in images
+    """
     mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh()
+    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1)
 
     image = cv.imread("danial-craig.jpg")
     if image is None:
         print("Failed to load image")
 
-    height, width, _ = image.shape
+    height, width = image.shape[:2]
     rgb_image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-    result = face_mesh.process(rgb_image)
+    with face_mesh as fm:
+        result = fm.process(rgb_image)
+    if not result.multi_face_landmarks:
+        return None
+
     for facial_landmarks in result.multi_face_landmarks:
         for i in range(0, 468):
             pt1 = facial_landmarks.landmark[i]
